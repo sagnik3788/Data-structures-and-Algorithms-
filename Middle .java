93f69ab71ecd6498d603dd2173413a31561d6b61
sagnik3788 <116512372+sagnik3788@users.noreply.github.com>
2023-03-25 14:32:20 +0530

    Add files via upload

---
+++ Middle .java
@@ -0,0 +1,11 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+      ListNode hare =head;
+      ListNode turtle=head;
+      while(hare!=null&&hare.next!=null){
+          hare=hare.next.next;
+          turtle=turtle.next;
+      }
+      return turtle;
+    }
+}
