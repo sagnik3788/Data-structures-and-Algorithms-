ac6a243ebb1b49b919d9d28fa29287c7805b1844
Sagnik Das <116512372+sagnik3788@users.noreply.github.com>
2023-05-31 15:50:04 +0530

    Update Function_Composition.js

--- Function_Composition.js
+++ Function_Composition.js
@@ -1,1 +1,17 @@
 
+       
+  var compose = function(functions) {
+  if(functions.length===0){
+     return function(x){
+        return x;
+     }
+  }
+    // Call each function in the array, passing in x as the argument.
+    return function(x){
+   var res = functions[functions.length - 1](x);  
+   for (var i = functions.length - 2; i >= 0; i--) {
+      res=functions[i](res);
+   }
+   return res;
+  };
+};
